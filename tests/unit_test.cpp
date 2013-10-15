7da23de880dba65c93e1615ee1bb1e4c29d49ab9
bitdewy <bitdewy@gmail.com>
2013-10-15 12:04:16 +0800

    add unit test

---
+++ tests/unit_test.cpp
@@ -0,0 +1,15 @@
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace unittest {
+
+TEST_CLASS(UnitTest) {
+ public:
+  TEST_METHOD(TestMethod) {
+    // TODO: Your test code here
+  }
+
+};
+
+}  // namespace unittest
