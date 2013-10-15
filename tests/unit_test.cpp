396d3928b790659b0a0cb7a50d319eba6921650a
bitdewy <bitdewy@gmail.com>
2013-10-15 15:24:32 +0800

    add copyright

--- tests/unit_test.cpp
+++ tests/unit_test.cpp
@@ -1,4 +1,10 @@
-#include "CppUnitTest.h"
+//  Copyright 2013, bitdewy@gmail.com
+//  Distributed under the Boost Software License, Version 1.0.
+//  You may obtain a copy of the License at
+//
+//  http://www.boost.org/LICENSE_1_0.txt
+
+#include "cppunittest.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
